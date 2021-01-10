a7ddb7ce996e441488534c4eb620305eee99ef9e
keke <ryehkk@yahoo.co.jp>
2021-01-10 15:31:20 +0900

    add router

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import TickerPage from './components/pages/TickerPage';
+import HomePage from './components/pages/HomePage';
 
-function App() {
-  return <div className="App"></div>;
-}
+const App: React.FC = () => {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/tickers" component={TickerPage} exact />
+        <Route path="/" component={HomePage} exact />
+      </Switch>
+    </Router>
+  );
+};
 
 export default App;
---
+++ src/components/pages/TickerPage.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const TickerPage: React.FC = () => {
+  return <div>ティッカー一覧</div>;
+};
+
+export default TickerPage;
