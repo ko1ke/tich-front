ff633c4fc1ba11ef46e74626403ff5e01d368ffc
keke <dondohare55@gmail.com>
2021-03-30 08:44:58 +0900

    remove jp fonts declaration

--- src/components/templates/GenericTemplate.tsx
+++ src/components/templates/GenericTemplate.tsx
@@ -11,25 +11,8 @@ import SideBar from '../molecules/SideBar';
 import UserSnack from '../molecules/UserSnack';
 
 const theme = createMuiTheme({
-  typography: {
-    fontFamily: [
-      'Noto Sans JP',
-      'Lato',
-      '游ゴシック Medium',
-      '游ゴシック体',
-      'Yu Gothic Medium',
-      'YuGothic',
-      'ヒラギノ角ゴ ProN',
-      'Hiragino Kaku Gothic ProN',
-      'メイリオ',
-      'Meiryo',
-      'ＭＳ Ｐゴシック',
-      'MS PGothic',
-      'sans-serif',
-    ].join(','),
-  },
   palette: {
-    primary: { main: colors.blue[800] }, // テーマの色
+    primary: { main: colors.blue[800] }, // theme color
   },
 });
 
