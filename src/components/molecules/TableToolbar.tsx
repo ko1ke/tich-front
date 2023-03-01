ea5cf8e19403514de30ecfa2850dfc5aae2a1e9e
keke <dondohare55@gmail.com>
2023-03-01 22:25:28 +0900

    mui5 to table tool bar

--- src/components/molecules/TableToolbar.tsx
+++ src/components/molecules/TableToolbar.tsx
@@ -1,39 +1,28 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import PostDialog from './PostDialog';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import TablePostDialog from './TablePostDialog';
+import type { Ticker } from '../../typings';
 
-const useToolbarStyles = makeStyles((theme) => ({
-  root: {
-    paddingLeft: theme.spacing(2),
-    paddingRight: theme.spacing(1),
-  },
-  title: {
-    flex: '1 1 100%',
-  },
-}));
+type Props = {
+  addHandler: (item: any) => void;
+  title: string;
+  tickers: Ticker[];
+};
 
 const TableToolbar: React.FC<Props> = ({
   addHandler,
   title,
   tickers,
 }: Props) => {
-  const classes = useToolbarStyles();
   return (
     <Toolbar>
-      <PostDialog addHandler={addHandler} tickers={tickers} />
-      <Typography className={classes.title} variant="h6">
+      <TablePostDialog addHandler={addHandler} tickers={tickers} />
+      <Typography sx={{ flex: '1 1 100%' }} variant="h6">
         {title}
       </Typography>
     </Toolbar>
   );
-};
-
-type Props = {
-  addHandler: (item: any) => void;
-  title: string;
-  tickers: any;
 };
 
 export default TableToolbar;
